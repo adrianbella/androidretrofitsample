8c809ae22c5716e3b442cc818c85f3c7b549b86e
adrian <bella.adus@gmail.com>
2018-07-26 16:06:07 +0200

    Uploading initial retrofit project!

---
+++ app/src/main/java/retrofit2sample/com/example/adrian/retrofitapp/APIInterface.java
@@ -0,0 +1,31 @@
+package retrofit2sample.com.example.adrian.retrofitapp;
+
+import retrofit2sample.com.example.adrian.retrofitapp.pojo.MultipleResource;
+import retrofit2sample.com.example.adrian.retrofitapp.pojo.User;
+import retrofit2sample.com.example.adrian.retrofitapp.pojo.UserList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+
+interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
+
